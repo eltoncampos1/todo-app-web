6b3a762780235e893cd0bc7bcf06966bdbc697ca
eltoncampos1 <eltoncampos36@gmail.com>
2021-12-13 22:55:54 -0300

    refactor: provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import { useState } from 'react'
 import { Layout } from './systems/core/components'
 import { Main } from './systems/Main'
 import { Header } from './systems/header/components'
-import { TodoProvider } from './context/todoContext'
 
 function App() {
 
   return (
-    <TodoProvider>
-      <Layout title='Todo App'>
-        <Header />
-        <Main />
-      </Layout>
-    </TodoProvider>
+
+    <Layout title='Todo App'>
+      <Header />
+      <Main />
+    </Layout>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,20 @@ import { HTML5Backend } from 'react-dnd-html5-backend'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 import App from './App'
+import { TodoProvider } from './context/todoContext'
 import { GlobalStyle } from './styles/globalStyles'
 import theme from './styles/theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </DndProvider>
+    <TodoProvider>
+      <DndProvider backend={HTML5Backend}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </DndProvider>
+    </TodoProvider>
   </React.StrictMode >,
   document.getElementById('root')
 )
